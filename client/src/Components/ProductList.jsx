e0b9e28204103d9c1d3edeb09e1b556546c234ae
ds.vasudev <vasudevds1729@gmail.com>
2023-12-29 21:06:45 +0530

    fetch efficiency improved

--- client/src/Components/ProductList.jsx
+++ client/src/Components/ProductList.jsx
@@ -7,21 +7,21 @@ import axios from "axios";
 import { NavLink } from "react-router-dom";
 import "../index.css";
 
-async function handleEvent(e) {
+async function handleEvent( e ) {
   e.preventDefault();
   const productDetails = {
     productID: e.target.value,
   };
 
-  const response = await fetch("https://roughage-api.vercel.app/api/addToCart", {
+  const response = await fetch( "http://localhost:3001/api/addToCart", {
     method: "POST",
     headers: {
       "content-type": "application/json",
     },
-    body: JSON.stringify(productDetails),
-  }).then((response) => {
-    if (response.status === 201) {
-      Swal.fire({
+    body: JSON.stringify( productDetails ),
+  } ).then( ( response ) => {
+    if ( response.status === 201 ) {
+      Swal.fire( {
         position: "top-end",
         width: 500,
         height: 100,
@@ -29,44 +29,44 @@ async function handleEvent(e) {
         title: "Product is Added to the Cart",
         showConfirmButton: true,
         timer: 990,
-      });
+      } );
     }
-  });
+  } );
 }
 
-function Product(props) {
+function Product( props ) {
   const [ buttonContent, setButtonContent ] = useState( '+' );
-  function showFullContent(e){
-    
-    setButtonContent("Quick Add")
+  function showFullContent( e ) {
+
+    setButtonContent( "Quick Add" )
   }
-  function showLessContent(e){
-    setButtonContent("+");
+  function showLessContent( e ) {
+    setButtonContent( "+" );
 
   }
   return (
     <>
       <NavLink
-        to={`/product/${props.id}`}
+        to={ `/product/${ props.id }` }
         className="col col-lg-3  col-md-6  col-sm-6 col-mb-6 col-xs-6"
-        style={{textDecoration:'none'}}
+        style={ { textDecoration: 'none' } }
       >
         <div className="cardS">
           <div className="cardI">
-            <img src={props.img} alt={props.productName} id="productImage" />
+            <img src={ props.img } alt={ props.productName } id="productImage" />
           </div>
           <div className="cardB">
             <div className="cartT">
-              <text id="name">{props.productName}</text>
+              <text id="name">{ props.productName }</text>
             </div>
             <div className="cardText">
-              <text id="price">Starting Price @ {props.cost}</text>
+              <text id="price">Starting Price @ { props.cost }</text>
             </div>
             <div className="cardButtonClass">
               <Button
-                onMouseOver={(e)=>showFullContent(e)}
+                onMouseOver={ ( e ) => showFullContent( e ) }
                 onMouseOut={ ( e ) => showLessContent( e ) }
-                name={props.productName}
+                name={ props.productName }
                 variant="success"
                 value={ props.id }
                 className="addToCartButton"
@@ -87,51 +87,62 @@ function Product(props) {
 
 
 function ProductList() {
-  const [productsAvailable, setProductsAvailable] = useState([]);
-  useEffect(() => {
-    axios
-      .get("https://roughage-api.vercel.app/api/getProducts")
-      .then((productsRecieved) => {
-        if (productsRecieved.status === 200) {
-          // Swal.fire( "Sucessfully loaded all the products", "", "success" );
-          setProductsAvailable(productsRecieved.data);
+  const [ productsAvailable, setProductsAvailable ] = useState( [] );
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:3001/api/getProducts")
+  //     .then((productsRecieved) => {
+  //       if (productsRecieved.status === 200) {
+  //         // Swal.fire( "Sucessfully loaded all the products", "", "success" );
+  //         setProductsAvailable(productsRecieved.data);
+  //       }
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, []);
+  useEffect( async () => {
+    const response = await fetch( "http://localhost:3001/api/getProducts" )
+      .then( async response => {
+        if ( response.status === 200 ) {
+          const data = await response.json();
+          console.log( data );
+          setProductsAvailable( data );
         }
-      })
-      .catch((err) => console.log(err));
-  }, []);
-  const productList = products.map((product) => (
+      } )
+      .catch( ( err ) => console.log( err ) );
+  }, [] )
+  const productList = products.map( ( product ) => (
     <Product
-      key={product.id}
-      id={product.id}
-      productName={product.name}
-      cost={product.cost}
-      img={product.img}
+      key={ product.id }
+      id={ product.id }
+      productName={ product.name }
+      cost={ product.cost }
+      img={ product.img }
     />
-  ));
-  const producedProducts = productsAvailable.map((producedProduct) => (
+  ) );
+  const producedProducts = productsAvailable.map( ( producedProduct ) => (
     <Product
-      key={producedProduct.productID}
-      id={producedProduct.productID}
-      productName={producedProduct.productName}
-      cost={producedProduct.price}
-      img={producedProduct.image}
-      description={producedProduct.description}
+      key={ producedProduct.productID }
+      id={ producedProduct.productID }
+      productName={ producedProduct.productName }
+      cost={ producedProduct.price }
+      img={ producedProduct.image }
+      description={ producedProduct.description }
     />
-  ));
+  ) );
   return (
     <div className="row allProducts">
-      {productsAvailable.map((prouctAvailable) => (
+      { productsAvailable.map( ( prouctAvailable ) => (
         <Product
-          key={prouctAvailable.productID}
-          id={prouctAvailable.productID}
-          productName={prouctAvailable.productName}
-          cost={prouctAvailable.price}
-          img={prouctAvailable.image}
-          description={prouctAvailable.description}
+          key={ prouctAvailable.productID }
+          id={ prouctAvailable.productID }
+          productName={ prouctAvailable.productName }
+          cost={ prouctAvailable.price }
+          img={ prouctAvailable.image }
+          description={ prouctAvailable.description }
         />
-      ))}
+      ) ) }
     </div>
   );
 }
 
-export { Product, ProductList } ;
+export { Product, ProductList };
