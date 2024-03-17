8fafff8833761a9c89a5778b267a93ca3c7cdd78
dsvasudev <vasudevds1729@gmail.com>
2024-03-17 10:43:29 +0530

    update

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,7 +4,9 @@ module.exports={
     "password": "root",
     "database": "roughage-ecommerce",
     "host": "127.0.0.1",
+    // "host": "localhost",
     "dialect": "mysql",
+    // "dialect": "postgres",
   },
   "test": {
     "username": "root",
@@ -13,20 +15,13 @@ module.exports={
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
-  // "production": {
-  //   "username": "root",
-  //   "password": null,
-  //   "database": "database_production",
-  //   "host": "127.0.0.1",
-  //   "dialect": "mysql"
-  // },
   "production": {
-    "use_env_variable": process.env.DATABASE_URL, // Use the DATABASE_URL environment variable for production
+    "use_env_variable": process.env.DATABASE_URL, 
     "dialect": 'postgresql', 
     "dialectOptions": {
       "ssl": {
         "require": true,
-        "rejectUnauthorized": false, // To avoid issues with self-signed certificates
+        "rejectUnauthorized": false, 
       },
     },
     "ssl": true,
