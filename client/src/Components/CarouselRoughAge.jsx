1126975cece3db49f08a78fb126c7f8269d75a88
DARSE SHIKARI VASUDEV <103886332+dsvasudev19@users.noreply.github.com>
2024-03-28 14:22:21 +0530

    Update CarouselRoughAge.jsx

--- client/src/Components/CarouselRoughAge.jsx
+++ client/src/Components/CarouselRoughAge.jsx
@@ -10,7 +10,7 @@ function CarouselRoughAge() {
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h3 className='carouselCaptionHeading'>Rooted in Nature, Bursting with Flavor: Explore Our Organic Wonderland 🌻🍊</h3>
+{/*                     <h3 className='carouselCaptionHeading'>Rooted in Nature, Bursting with Flavor: Explore Our Organic Wonderland 🌻🍊</h3> */}
                     {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
                 </Carousel.Caption>
             </Carousel.Item>
@@ -21,7 +21,7 @@ function CarouselRoughAge() {
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h3 className='carouselCaptionHeading'>Where Freshness Meets Flavor: Embrace the Organic Lifestyle!</h3>
+{/*                     <h3 className='carouselCaptionHeading'>Where Freshness Meets Flavor: Embrace the Organic Lifestyle!</h3> */}
                     {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
                 </Carousel.Caption>
             </Carousel.Item> 
@@ -32,7 +32,7 @@ function CarouselRoughAge() {
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h3 className='carouselCaptionHeading'>Nature's Finest Harvest: Handpicked and Delivered with Love 🌱🍓</h3>
+{/*                     <h3 className='carouselCaptionHeading'>Nature's Finest Harvest: Handpicked and Delivered with Love 🌱🍓</h3> */}
                     {/*<p>
                         Praesent commodo cursus magna, vel scelerisque nisl consectetur.
                     </p> */}
